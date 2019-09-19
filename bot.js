d7afa92145b6343438aaffa08a666f9bf0924208
Lena Schimmel <lena@serioese.gmbh>
2019-09-19 21:08:28 +0200

    Async wrappers upload_image and post_tweet instead of callbacks.

--- bot.js
+++ bot.js
@@ -1,47 +1,53 @@
-fs = require('fs');
-path = require('path');
-Twit = require('twit');
-var multer  = require('multer');
+const fs = require('fs');
+const path = require('path');
+const Twit = require('twit');
+const util = require('util');
+const multer  = require('multer');
 
 config = require(path.join(__dirname, 'config.js'));
+const readFile = util.promisify(fs.readFile);
 
 var upload = multer({ storage: multer.memoryStorage() })
 var T = new Twit(config);
 
-function upload_image(image_path, text) {
+async function upload_image(image_path, alt_text) {
     console.log('Opening an image...');
-    var b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+    var b64content = await readFile(image_path, { encoding: 'base64' });
 
     console.log('Uploading an image...');
 
-    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-        if (err){
-        console.log('ERROR:');
-        console.log(err);
-        }
-        else{
-        console.log('Image uploaded!');
-        console.log('Now tweeting it...');
-
-        T.post('statuses/update', {
-            status: text,
-            media_ids: new Array(data.media_id_string)
-        },
-            function(err, data, response) {
-            if (err){
-                console.log('ERROR:');
-                console.log(err);
-            }
-            else{
-                console.log('Posted an image!');
-            }
-            }
-        );
-        }
-    });
+    return new Promise(function(resolve, reject) { 
+        T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+            if(err)
+                reject(err);
+            else 
+                resolve(data);
+        });
+     });
 }
 
-var text = "Die Twitter-Schnittstelle wird gerade getestet.";
-upload_image("logo.png", text);
+async function post_tweet(text, mediaArray) {
+    console.log('Sending a tweet...');
 
-console.log("Running.");
+    return new Promise(function(resolve, reject) { 
+        T.post('statuses/update', {
+            status: text,
+            media_ids: mediaArray
+        },
+        function (err, data, response) {
+            if(err)
+                reject(err);
+            else 
+                resolve(data);
+        });
+     });
+}
+
+
+(async () => {
+    var text = "Die Twitter-Schnittstelle wird gerade doppelt asynchron getestet.";
+    var media = await upload_image("logo.png", "Das Logo von produkt_warnung.");
+    var result = await post_tweet(text, new Array(media.media_id_string));
+    
+    console.log("Running.");
+})();
